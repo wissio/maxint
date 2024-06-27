91bf4d3cb81a6393e9f1c4498111615528463cbc
wissio <wissio178@gmail.com>
2024-06-27 12:52:20 +0300

    changed main.go with race condition

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
 func main() {
-    var wg sync.WaitGroup
-    wg.Add(5)
+	var wg sync.WaitGroup
+	wg.Add(5)
 
-    for i := 0; i < 5; i++ {
-        go func() {
-            fmt.Println(i)
-            wg.Done()
-        }()
-    }
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
 
-    wg.Wait()
+	wg.Wait()
 }
 
 func MaxInt(a, b int) int {
-    if a >= b {
-        return a
-    }
+	if a >= b {
+		return a
+	}
 
-    return b
-} 
+	return b
+}
